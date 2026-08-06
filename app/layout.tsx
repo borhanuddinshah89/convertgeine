0e0149e467f28f6d852d4b8930cab91f132335d7
Borhan Uddin <borhanuddinshah89@gmail.com>
2026-08-06 03:05:30 -0500

    Add Google AdSense verification code

--- app/layout.tsx
+++ app/layout.tsx
@@ -109,6 +109,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en-CA">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9419298891554328"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="bg-slate-950 text-white antialiased">
         {children}
         <Footer />
